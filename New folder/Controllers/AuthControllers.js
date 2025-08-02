67e81d17b3524b1a7c84faa4deb5434224715b4d
coder-ali-ba <habibali8770@gmail.com>
2025-08-02 22:58:16 +0500

    changes

--- New folder/Controllers/AuthControllers.js
+++ New folder/Controllers/AuthControllers.js
@@ -1,12 +1,18 @@
+import { response } from "express"
 import allUsers from "../models/AuthModels.js"
 import bcrypt from "bcryptjs"
 
 const signUpController = async(req , res) => { 
     const body = req.body   
     const {email , password} = body
+    if (!email || !password) {
+      return res.status(400).json({ 
+        message: "Email and password are required"
+     });
+    }
     const checkUser = await allUsers.findOne({email})
     if(checkUser){
-        return res.json({
+        return res.status(409).json({
             message : "user already exist"
         })
     }
@@ -15,19 +21,38 @@ const signUpController = async(req , res) => {
 
     const userObj = {
         ...req.body,
-        password : hashPass
+        password : hashPass,
+        userType : "user"
     }
     const createUser = await allUsers.create(userObj)
 
     res.json({
         data : createUser,
-        message : "got signup api",        
+        message : "Signed Up Successfully",        
     })
 }
 
-const logInController = (req , res) => {   
-    res.json({
-        message : "got login api",
+
+
+
+const logInController = async(req , res) => {   
+    const{email , password} = req.body;
+    const checkEmail = await allUsers.findOne({email})
+    if(!checkEmail){
+        return res.status(404).json({
+            message : "Invalid Email or Password"
+        })
+    }
+
+    const checkPass =await bcrypt.compare(password , checkEmail.password)
+    if(!checkPass){
+        return res.status(404).json({
+            message : "Invalid Email or Password"
+        })
+    }
+    res.status(200).json({
+        message : "Logged in successfully",
+        data : checkEmail
     })
 }
 
